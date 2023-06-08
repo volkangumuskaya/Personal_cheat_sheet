c279ecc46b7b057bfbe28a223119ef7c9fc16ff3
volkangumuskaya <54629964+volkangumuskaya@users.noreply.github.com>
2023-06-08 15:20:56 +0200

    Update intro.py

--- intro.py
+++ intro.py
@@ -8,7 +8,6 @@ for name in dir():
     if not name.startswith('_'):
         del locals()[name]
 
-
 import numpy as np
 import pandas as pd
 import os
@@ -43,9 +42,7 @@ air_q=pd.read_csv(dir_path + '\\raw_files\\' + file_name)
 
 
 #Sort for convenience (not really necessary)
-iris.describe()
 df=iris.copy()
-
 data_steps=[]
 data_steps.append(['iris',len(df.columns),len(df),'Raw_data'])
 
@@ -119,3 +116,328 @@ df.drop_duplicates(subset=['int_class'])
 df[df.duplicated(subset=['int_class'],keep=False)]
 df[~df.duplicated(subset=['int_class'],keep='first')]
 df[~df.duplicated(subset=['int_class'],keep='last')]
+
+# count per group with a condition
+df['nonNL']=(df['Dest-cntry'] != 'NL').groupby(df['Orig-cntry']).transform('sum')
+
+#find max per group row-wise
+df['max_total'] = pd.concat([df['sepal_length_cm'] + df['sepal_width_cm'],
+                             df['petal_length_cm'] + df['petal_width_cm']],
+                                    axis=1).max(axis=1)
+
+#groupby example
+
+cond_positive_target=df.target>0
+df_positive_target_totals = df[cond_positive_target].groupby('int_class',observed=True).\
+    agg({'sepal_length_cm': 'sum',
+         'petal_length_cm': 'sum',}).\
+    reset_index().\
+    rename(columns={'sepal_length_cm':'sum_sepal',
+                    'petal_length_cm':'sum_petal'})
+
+#groupby sums
+df['sum_sepal_lengths_per_int_class'] = df.groupby('int_class')['sepal_length_cm'].transform('sum')
+df['sum_sepal_lengths_per_int_class_non_zero_target'] = df['sepal_length_cm'].where(df['target'] >0).groupby(df['int_class']).transform(
+    'sum')
+
+#compare floats
+df['Compare_lengths_tol_0.1']=np.where(np.isclose(df.sepal_length_cm,df.petal_length_cm,atol=0.1),1,0)
+df['Compare_lengths_tol_3']=np.where(np.isclose(df.sepal_length_cm,df.petal_length_cm,atol=3),1,0)
+
+#rename columns
+df=df.rename(columns={'Orig-cntry': "origin_country", 'Dest-cntry': "dest_country"})
+df.columns
+
+#cuts
+df['cut']=pd.cut(df.sepal_length_cm,bins=[0,1,2,3,4,5,6,7,np.inf])
+df['cut_lower']=pd.cut(df.sepal_length_cm,bins=[0,1,2,3,4,5,6,7,np.inf]).map(lambda x: x.left)
+df['cut_upper']=pd.cut(df.sepal_length_cm,bins=[0,1,2,3,4,5,6,7,np.inf]).map(lambda x: x.right)
+df['cut'].value_counts()
+
+
+
+####################################
+###############PLOTS################
+####################################
+
+#Histogram
+import plotly.express as px
+import plotly.io as pio
+pio.renderers='svg'
+df.sepal_length_cm.hist(bins=30, facecolor='green', alpha=0.75)
+
+#Histogram detailed with px
+fig=px.histogram(df.sepal_length_cm,title="Histogram")
+
+fig.update_xaxes(title_text="sepal_length_cm")
+fig.update_yaxes(title_text="n")
+fig.update_layout(
+    xaxis = dict(
+        tickmode = 'linear',
+        tick0 = 2,
+        dtick = 0.5
+    ),bargap=0.01
+)
+fig.update_xaxes(showgrid=True)
+
+fig.update_yaxes(showgrid=True)
+fig.update_xaxes(range=[2,10])
+fig.update_yaxes(range=[0,50])
+fig.update_traces(marker_color='green')
+fig.show()
+
+# Box plot with plotly
+px.box(df,\
+       x="origin_country", y="sepal_length_cm", color="target",\
+       facet_col='dest_country',\
+       # category_orders={'DT_name':['dt_before_postcodes','dt_before_corridor','dt']},\
+       points='all').show()
+
+# Bar abd scatter chart together
+df['flower_id']=np.arange(0,len(df))
+
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(
+    go.Bar(x=df.flower_id, y=df.sepal_width_cm,
+           # text=round(metrics.R2, 2),
+           marker_color='light blue', opacity=0.7,
+           name="Sepal_width"),
+    secondary_y=False
+)
+
+fig.add_trace(
+    go.Scatter(x=df.flower_id, y=df.sepal_length_cm,
+               mode='markers', name="Sepal length"),
+    secondary_y=True
+)
+#Optional line
+fig.add_trace(
+    go.Scatter(x=[0,150], y=[0,5])
+)
+
+fig.update_traces(marker=dict(size=10,
+                              line=dict(width=1.5, color='black')),
+                  selector=dict(mode='markers'))
+fig.update_layout(
+    title="Bar and scatter",
+    xaxis_title="Flower ID",
+    yaxis_title="Length",
+    legend_title="Trace type",
+)
+fig.update_yaxes(title_text="Length_2", secondary_y=True)
+fig.update_yaxes(range=[0, 5], secondary_y=False)
+fig.update_yaxes(range=[0, 10], secondary_y=True)
+fig.show()
+
+#save plotly figure
+import plotly.offline
+plotly.offline.plot(fig,filename='images\\'+'test.html',auto_open=False)
+
+# Scatter plot with size and custom markers
+fig = go.Figure(
+    data=[go.Scatter(x=df.flower_id, y=df.sepal_length_cm, text=df.int_class,
+                     # textposition="top center",
+                     mode='markers+text',
+                    textposition='middle center',
+                     showlegend=False,
+                     marker=dict(
+                         size=df.petal_length_cm,
+                         color=df.target,
+                         sizemode='area',
+                         sizeref=2. * max(df.petal_length_cm,) / (40. ** 2),
+                         sizemin=0,
+                         showscale=True,
+                         colorscale=[(0, "green"), (0.5, "yellow"), (1, "red")],
+                         colorbar=dict(orientation='v', title='target')
+                     )
+                     ),
+          ]
+)
+
+fig.update_layout(title="Title",
+                  xaxis_title='Flower_id',
+                  yaxis_title='Sepal length')
+# fig.update_layout(coloraxis_colorbar=dict(orientation="h"))
+# fig.update_layout(legend_orientation="h")
+# fig.layout.update(xaxis_range=[0, 150])
+# fig.layout.update(yaxis_range=[0, 8])
+fig.show()
+
+#Subplots with different types
+fig = make_subplots(1, 2, specs=[[{"secondary_y": True}, {"secondary_y": True}]],
+                    shared_yaxes='all')
+
+fig.add_trace(
+    go.Scatter(x=df.flower_id, y=df.sepal_length_cm,
+               mode='lines+markers',
+               marker=dict(color='red',
+                           size=df.int_class,
+                           sizeref=max(df.int_class) / (4.5 ** 2),
+                           sizemin=0),
+               name='Sepal length'),
+    row=1,
+    col=1,
+    secondary_y=False
+)
+fig.add_trace(
+    go.Bar(x=df.flower_id, y=df.int_class,
+           opacity=0.4,marker={'color':'navy'},
+           name='Class'),
+    row=1,
+    col=1,
+    secondary_y=True
+)
+fig.add_trace(
+    go.Scatter(x=df.flower_id, y=df.petal_length_cm,
+               mode='markers',
+               marker=dict(color='red',
+                           size=df.int_class,
+                           sizeref=max(df.int_class) / (4.5 ** 2),
+                           sizemin=0),
+               name='Petal length'),
+    row=1,
+    col=2,
+    secondary_y=False
+)
+fig.add_trace(
+    go.Bar(x=df.flower_id, y=df.int_class,
+           opacity=0.4,marker={'color':'navy'},
+           name='Class'),
+    row=1,
+    col=2,
+    secondary_y=True
+)
+fig.update_yaxes(showgrid=False, secondary_y=True,range=[0,20])
+fig.show()
+
+#Scatter plot with ordered x-axis
+
+x_categories= df.sort_values('int_class').flower_id.to_list()
+
+fig=px.scatter(df,x='flower_id',y='petal_length_cm',
+               size='int_class',color='int_class',color_continuous_scale='ylorrd')
+fig.update_xaxes(type="category",categoryorder='array', categoryarray= x_categories)
+fig.update_xaxes(tickangle=90)
+fig.update_traces(marker=dict(line=dict(width=1.5, color='black')))
+fig.show()
+
+##Correlations
+from scipy.stats import pearsonr
+pearsonr(df.sepal_length_cm, df.petal_length_cm)
+df.sepal_length_cm.corr(df.petal_length_cm)
+
+#weighted correlation
+def linear_fit_func(x, a, b):
+    return a + b * x
+
+def m_weighted_corr(x, w):
+    """Weighted Mean"""
+    return np.sum(x * w) / np.sum(w)
+
+def cov_weighted_corr(x, y, w):
+    """Weighted Covariance"""
+    return np.sum(w * (x - m_weighted_corr(x, w)) * (y - m_weighted_corr(y, w))) / np.sum(w)
+
+def weighted_corr(x, y, w):
+    """Weighted Correlation"""
+    return cov_weighted_corr(x, y, w) / np.sqrt(cov_weighted_corr(x, x, w) * cov_weighted_corr(y, y, w))
+
+weighted_corr(df.dropna().sepal_length_cm,
+              df.dropna().petal_length_cm,
+              len(df.dropna())*[1])
+
+#normality
+from scipy import stats
+norm_test_arr = np.random.randn(100000)
+stats.shapiro(norm_test_arr)
+
+
+#Train model
+from sklearn import datasets
+import numpy as np
+import pandas as pd
+
+# Load the iris dataset
+iris = datasets.load_iris()
+iris = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
+                     columns= iris['feature_names'] + ['target'])
+df=iris.copy()
+df['target']=df['target'].astype('category')
+
+# Train LGBM model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+import lightgbm as lgb
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+
+lgbm_params = {
+    'n_estimators': 10,  # 100, opt 1000
+    'max_depth': 6,  # 6, opt 14
+    'learning_rate': 0.5,  # 0.3
+    'reg_alpha': 0.5,  # none
+    'reg_lambda': 0,  # none,
+    # 'monotone_constraints': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #This is used when we want monotonic constraints for example for regression wrt a feature
+}
+
+features = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+target = "target"
+
+#Change string and object typ[e columns to catgeory for LGBM
+for col in df.columns:
+    col_type = df[col].dtype
+    if col_type == 'object' or col_type.name == 'string':
+        df[col] = df[col].astype('category')
+df.dtypes
+
+X = df[features].copy()  # Features table
+y = df[target]  # Target table (Natural logarithm of the target is used!)
+
+# Split randomly
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=.20,random_state=42)
+
+# Train model
+# lgbr = lgb.LGBMRegressor(**lgbm_params)  # lgbr.get_params()
+lgbr = lgb.LGBMClassifier(**lgbm_params)  # lgbr.get_params()
+lgbr.fit(X_train, Y_train, eval_set=(X_test, Y_test), feature_name='auto', categorical_feature='auto',verbose=1)
+
+# predictions on log scale
+pred_test = lgbr.predict(X_test)
+pred_train = lgbr.predict(X_train)
+
+#Accuracy
+acc_test=(pred_test==Y_test).sum()/len(Y_test)
+acc_train=(pred_train==Y_train).sum()/len(Y_train)
+
+# Collect Performance metrics on the trained model (not meaning for classification but anyway))
+# R2_test = lgbr.score(X_test, Y_test)  # R2 on test set
+# MAPE = np.mean((Y_test - pred_test) / Y_test)  # MAPE on original values
+# MAE = (np.mean((Y_test - pred_test)))  # MAE on test values in percentage
+
+#Hyperparameter tuning
+search_params = {
+    'n_estimators' : [5,10,50],  # 100
+    'max_depth' : [6,10,15],  # 6
+    'learning_rate' : [0.3,0.5],  # 0.3
+    }
+
+# Randomized search
+lgb_CV = RandomizedSearchCV(estimator=lgb.LGBMClassifier(), param_distributions=search_params,
+                            scoring='r2', cv=4, verbose=10, n_iter=12)
+lgb_CV.fit(X_train, Y_train)
+RandomizedSearchCV_df=pd.DataFrame(lgb_CV.cv_results_).sort_values('rank_test_score')
+
+# Grid search
+lgb_CV = GridSearchCV(estimator=lgb.LGBMClassifier(), param_grid=search_params,
+                                            scoring='r2', cv=5,verbose=10)
+lgb_CV.fit(X_train, Y_train)
+GridSearchCV_df=pd.DataFrame(lgb_CV.cv_results_).sort_values('rank_test_score')
+
+
+
